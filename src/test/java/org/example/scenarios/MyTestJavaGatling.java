2fc781c5b81c401893e7b5cf4148cd2baa16e0b1
Dmytro Pychyk <amid.kichip@gmail.com>
2024-02-29 20:04:04 +0100

    refactoring

--- src/test/java/org/example/scenarios/MyTestJavaGatling.java
+++ src/test/java/org/example/scenarios/MyTestJavaGatling.java
@@ -14,7 +14,7 @@ import static io.gatling.javaapi.core.CoreDsl.scenario;
 import static io.gatling.javaapi.core.OpenInjectionStep.atOnceUsers;
 import static io.gatling.javaapi.http.HttpDsl.http;
 
-//My test
+//My test 1
 public class MyTestJavaGatling extends Simulation {
     HttpProtocolBuilder httpProtocol = http
             .baseUrl("http://computer-database.gatling.io")
